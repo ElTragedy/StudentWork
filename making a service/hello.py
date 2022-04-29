05f4971b94b436e913d13c046ef980dde1f07eef
ElTragedy <aaron_sierra1@baylor.edu>
2022-04-29 18:52:11 -0500

    works with python 3

--- making a service/hello.py
+++ making a service/hello.py
@@ -1,52 +1,60 @@
-#connect to a minecraft server and cout works if the connection worked exit 0
-#if the connection failed exit 1
-#!/usr/bin/env python3
-# Path: making a service\hello.py
-
-import struct
+#connect to a minecraft server using sockets and python 3
 import socket
+import sys
 import time
 import urllib
-try:
-    import urllib.request as urllib2
-except ImportError:
-    import urllib2
-import json
+import struct
 
-host = "192.168.7.222"
-port = 25565
+
+HOST = '192.168.7.222'
+PORT = '25565'
+BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-#log into mincraft.net first
+s.connect((HOST, PORT))
 
-logindata = {'user': 'aaronsierra2nd@yahoo.com', 'password': 'M1crosoft002873', 'version': '1.8.9'}
-data = urllib.urlencode(logindata)
-print('attempting to log into mincraft.net')
-req = urllib2.Request('https://login.minecraft.net/', data)
-response = urllib2.urlopen(req)
-returndata = response.read()
-returndata = returndata.split(':')
-mcsessionid = returndata[3]
-del req
-del returndata
+#log into minecraft.net
+loginInformation = {'user':'ElTragedyyy','password':'M1crosoft002873','version':'1.15.2'}
+data = urllib.parse.urlencode(loginInformation)
+print("attempting to log into minecraft.net")
+#make a request to 'https://login.minecraft.net', send the data, and get the response
+response = urllib.request.urlopen('https://login.minecraft.net', data.encode('utf-8'))
+SiteResponse = response.read()
+SiteResponse = SiteResponse.split(':')
+mcsessionid = SiteResponse[2]
 print("session id: " + mcsessionid)
-data = {'user':u'aaronsierra2nd@yahoo.com','host':u'192.168.7.222','port':u'25565','session':u'%s' % mcsessionid}
+data = {'user':'ElTragedyyy','host':u'127.0.0.1','port':25565}
 
-enc_user = data['user'].encode('utf-16BE')
-stringfmt = u'%(user)s:%(hosts)s:%(port)d'
+stringfmt = u'%(user)s:%(host)s:%(port)s'
 string = stringfmt % data
 structfmt = '>bh'
-packfmt = '>bih{}shiibBB'.format(len(enc_user))
-packetbytes = struct.pack(packfmt,1,23,len(data['user']),enc_user,0,0,0,0,0)
+packetbytes = struct.pack(structfmt, 2, len(string))+string.encode('utf-16be')
 s.send(packetbytes)
-connhash = s.recv(1024)
-print("conneciton hash: " + connhash)
-print ("sending handshake")
-req = urllib.urlopen('http://session.minecraft.net/game/joinserver.jsp?user=ElTragedyyy&sessionId=' + mcsessionid + '&serverId=' + connhash)
-returndata = req.read()
-if (returndata == 'OK'):
-    print("connection successful")
-    exit(0)  
+connhash = s.recv(BUFFER_SIZE)
+print("connection hash: " + connhash.decode('utf-16be'))
+print("sending handshake")
+if(SiteResponse == 'OK'):
+    print("logged in, now sending data to server")
 else:
-    print("connection failed")
-    exit(1)
+    print("Oops, something went wrong")
+time.sleep(5)
+
+enc_user = data['user'].encode('utf-16be')
+packfmt = '>bih{}shiibBB'.format(len(enc_user))
+packetbytes = struct.pack(packfmt,1,23,len(data['user']), enc_user,0,0,0,0,0,0)
+print(len(packetbytes))
+print("sending " + packetbytes + ' to server')
+s.send(packetbytes)
+
+#if the server responds with a packet, print it and exit 0
+#else print an error and exit 1
+if(s.recv(BUFFER_SIZE)):
+    print("received packet")
+    sys.exit(0)
+else:
+    print("error")
+    sys.exit(1)
+
+
+
+
+
